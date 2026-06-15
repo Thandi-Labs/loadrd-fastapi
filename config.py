e9d285232277822722523a40420afe84c1c7457b
njeruthuo <juliusn411@gmail.com>
2026-06-15 16:41:54 +0300

    fix: added patches for deployment

--- config.py
+++ config.py
@@ -1,4 +1,5 @@
 from pydantic_settings import BaseSettings
+from pydantic import ConfigDict
 
 
 class Settings(BaseSettings):
@@ -6,6 +7,12 @@ class Settings(BaseSettings):
     secret_key: str
     algorithm: str
     access_token_expire_minutes: int = 20
+
+    postgres_user: str
+    postgres_password: str
+    postgres_db: str
+
+    # model_config = ConfigDict(extra='ignore', env_file='.env')
 
     class Config:
         env_file = ".env"
