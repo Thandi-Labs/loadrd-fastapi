be1e693e7f338e1bae2379dc55e59bd133206961
njeruthuo <juliusn411@gmail.com>
2026-06-19 10:06:41 +0300

    feat: added a deactivate function

--- database.py
+++ database.py
@@ -6,7 +6,13 @@ from config import settings
 
 SQLALCHEMY_DATABASE_URL = settings.database_url
 
-engine = create_engine(SQLALCHEMY_DATABASE_URL)
+engine = create_engine(
+    SQLALCHEMY_DATABASE_URL,
+    pool_pre_ping=True,
+    pool_recycle=3600,
+    pool_size=10,
+    max_overflow=20
+)
 
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
